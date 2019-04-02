7ff3859097c35c9c96643623a0fc5e35889f57f9
lvhlucas <lvhlucas@gmail.com>
2019-04-02 11:52:36 -0300

    debug false

--- somartech/settings.py
+++ somartech/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&33ddqiz=gv@zx*npm&04yj$jiuypv20oyqw=&qtid1bk=kyaf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
